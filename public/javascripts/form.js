bb177613acdcb229f535143387bd6ac6d72e8d3d
Your Name <stephen.creedon@rapidappsgroup.com>
2011-07-17 23:02:53 +0100

    added from js

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -3,6 +3,7 @@ html.push("<form id='postcard-form'");
 html.push("<p><input style='width: 100%;' placeholder='your email address' type='text' id='from' /></p>");
 html.push("<p><input style='width: 100%;' placeholder='email this postcard to' type='text' id='to' /></p>");
 html.push("<p><textarea style='width: 100%; height: 100px;' placeholder='your message' id='message'></textarea></p>");
+html.push("<input type='submit'/>");
 html.push("</form>");
 
 $("#postcard-form").html(html.join(""));
