23ac5620b2601ffe1a27322ef65dd30e29fa940b
Your Name <stephen.creedon@rapidappsgroup.com>
2011-07-17 23:12:21 +0100

    added from js

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -3,9 +3,19 @@ html.push("<form id='postcard-form'");
 html.push("<p><input style='width: 100%;' placeholder='your email address' type='text' id='from' /></p>");
 html.push("<p><input style='width: 100%;' placeholder='email this postcard to' type='text' id='to' /></p>");
 html.push("<p><textarea style='width: 100%; height: 100px;' placeholder='your message' id='message'></textarea></p>");
-html.push("<input type='submit'/>");
 html.push("</form>");
+html.push("<button id='send-message-button'>send postcard</button>");
 
 $("#postcard-form").html(html.join(""));
 
 $(' [placeholder] ').defaultValue();
+$("#send-message-button").click(function(){
+	
+	$.ajax({
+		type: 'POST',
+		 url: 'www.doglight.co.uk/postacards/brighton',
+		 data: $('#postcard-form').serialize(),
+		 success: function(){$.growlUI('Growl Notification', 'your postcard has been sent')}
+	})
+	
+})
