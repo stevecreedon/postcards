a973fa1f3ffb42c98ee9da4d5b2f9105e36e027a
Your Name <stephen.creedon@rapidappsgroup.com>
2011-07-17 23:51:41 +0100

    added from js

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -1,5 +1,5 @@
 html = new Array()
-html.push("<form id='postcard-data'");
+html.push("<form id='postcard-data'>");
 html.push("<p><input style='width: 100%;' placeholder='your email address' type='text' id='from' /></p>");
 html.push("<p><input style='width: 100%;' placeholder='email this postcard to' type='text' id='to' /></p>");
 html.push("<p><textarea style='width: 100%; height: 100px;' placeholder='your message' id='message'></textarea></p>");
@@ -10,7 +10,7 @@ $("#postcard-form").html(html.join(""));
 
 $(' [placeholder] ').defaultValue();
 $("#send-message-button").click(function(){
-	alert($('#postcard-data').serialize());
+	consloe.log($('#postcard-data').serialize());
 	$.ajax({
 		type: 'POST',
 		 url: 'http://www.doglight.co.uk/postcards',
