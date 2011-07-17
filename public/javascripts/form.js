db34834b8ca03a3f58fbbf7b134e1f6c9dd495ea
Your Name <stephen.creedon@rapidappsgroup.com>
2011-07-17 23:55:31 +0100

    added from js

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -1,8 +1,8 @@
 html = new Array()
 html.push("<form id='postcard-data'>");
-html.push("<p><input style='width: 100%;' placeholder='your email address' type='text' id='from' /></p>");
-html.push("<p><input style='width: 100%;' placeholder='email this postcard to' type='text' id='to' /></p>");
-html.push("<p><textarea style='width: 100%; height: 100px;' placeholder='your message' id='message'></textarea></p>");
+html.push("<p><input style='width: 100%;' placeholder='your email address' type='text' name='from'/></p>");
+html.push("<p><input style='width: 100%;' placeholder='email this postcard to' type='text' name='to' /></p>");
+html.push("<p><textarea style='width: 100%; height: 100px;' placeholder='your message' name='message'></textarea></p>");
 html.push("</form>");
 html.push("<button id='send-message-button'>send postcard</button>");
 
