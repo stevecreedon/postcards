4fbe9f9c4fc19b8cd67afff049bb86e2ede9347d
Your Name <stephen.creedon@rapidappsgroup.com>
2011-07-18 00:12:48 +0100

    added from js

--- app/mailers/postcard.rb
+++ app/mailers/postcard.rb
@@ -3,7 +3,7 @@ class Postcard < ActionMailer::Base
   
   def basic(from, to, message)
     @message = message
-    @from = from
+    @sender = from
     mail(:to => to, :from => from, :bcc => ["steve@light-art.co.uk"], :subject => "Blake and #{from} have sent you a postcard from Brighton") do |format|
        format.html        
     end
