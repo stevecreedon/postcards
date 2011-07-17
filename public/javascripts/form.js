4145c5292822b42174042310c5509da5b6f8ff94
Your Name <stephen.creedon@rapidappsgroup.com>
2011-07-17 22:22:31 +0100

    added from js

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -1,5 +1,6 @@
 html = new Array()
-html.push("<p>send postcard to:<br/><input style='width: 100%;' type='text' id='message-to' /></p>")
-html.push("<p>message:<br/><textarea style='width: 100%;'  id='message-text'></textarea></p>")
+html.push("<p><input style='width: 100%;' type='text' id='message-to' value='your email address' /></p>");
+html.push("<p><input style='width: 100%;' type='text' id='message-to' value='email this postcard to'/></p>");
+html.push("<p>message:<br/><textarea style='width: 100%; height: 100px;'  id='message-text'>personal message</textarea></p>");
 
 $("#postcard-form").html(html.join(""))
