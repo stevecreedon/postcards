f3d7f9869b50e5c49c85d14e9d1ed311c2ad1a60
Your Name <stephen.creedon@rapidappsgroup.com>
2011-07-18 13:12:56 +0100

    added form view and controoler

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Postcards::Application.routes.draw do
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   resources :postcards
+  resources :forms
   # Sample resource route with options:
   #   resources :products do
   #     member do
