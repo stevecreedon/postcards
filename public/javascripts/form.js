0852739331de41de9ad26d75d326393ec33de530
Your Name <stephen.creedon@rapidappsgroup.com>
2011-07-18 13:19:14 +0100

    added form view and controoler

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -1,15 +1,3 @@
-html = new Array()
-html.push("<iframe>")
-html.push("<form id='postcard-data'>");
-html.push("<p><input style='width: 100%;' placeholder='your email address' type='text' name='from'/></p>");
-html.push("<p><input style='width: 100%;' placeholder='email this postcard to' type='text' name='to' /></p>");
-html.push("<p><textarea style='width: 100%; height: 100px;' placeholder='your message' name='message'></textarea></p>");
-html.push("</form>");
-html.push("<button id='send-message-button'>send postcard</button>");
-html.push("</iframe>")
-
-$("#postcard-form").html(html.join(""));
-
 $(' [placeholder] ').defaultValue();
 $("#send-message-button").click(function(){
 	$.ajax({
