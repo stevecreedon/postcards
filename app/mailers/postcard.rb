17ab282077d9b1f0b95ab98a4b549eb0448bf4a1
Your Name <stephen.creedon@rapidappsgroup.com>
2011-07-19 17:21:24 +0100

    removed ajax

--- app/mailers/postcard.rb
+++ app/mailers/postcard.rb
@@ -1,7 +1,7 @@
 class Postcard < ActionMailer::Base
   default :from => "no-reply@light-art.co.uk"
   
-  def basic(from, to, message)
+  def basic(to, from, message)
     @message = message
     @sender = from
     mail(:to => to, :from => from, :bcc => ["steve@light-art.co.uk"], :subject => "Blake and #{from} have sent you a postcard from Brighton") do |format|
