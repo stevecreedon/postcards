c33ce658b98153f69e884cfa17ba7a3cd6feb183
Your Name <stephen.creedon@rapidappsgroup.com>
2011-07-19 09:03:47 +0100

    added action to form

--- app/controllers/postcards_controller.rb
+++ app/controllers/postcards_controller.rb
@@ -8,7 +8,6 @@ class PostcardsController < ApplicationController
   
   def create
     Postcard.basic(params[:to], params[:from], params[:message]).deliver
-    render :partial => 'create'
   end
   
 end
